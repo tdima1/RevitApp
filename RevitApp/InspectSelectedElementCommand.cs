1f085a0083459a4d71790855f4985da7d01bf69e
Dima Tiberiu <dima.tiberiu1@gmail.com>
2019-07-02 16:08:00 +0300

    saved changes. to do tomorrow: string builder for data. instance/type(if and if not familyInstance,familySymbol), formula(isreadonly)

--- RevitApp/InspectSelectedElementCommand.cs
+++ RevitApp/InspectSelectedElementCommand.cs
@@ -3,7 +3,6 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using System;
-using System.Collections.Generic;
 using System.Text;
 
 namespace RevitApp
@@ -17,7 +16,7 @@ namespace RevitApp
          Document doc = uidoc.Document;
          Selection sel = uidoc.Selection;
          Element selectedElement = null;
-         string data;
+         StringBuilder data;
          //BindingMap bindings = doc.ParameterBindings;
          //DefinitionBindingMapIterator it = bindings.ForwardIterator();
          //
@@ -26,6 +25,7 @@ namespace RevitApp
 
          Reference selectedObjRef = sel.PickObject(ObjectType.Element, "Select one element");
          sel.Dispose();
+
          if (selectedObjRef != null) {
             selectedElement = doc.GetElement(selectedObjRef.ElementId);
          }
@@ -34,19 +34,31 @@ namespace RevitApp
 
          data = $"Element ID: {selectedElement.Id} \nElement Category Name: {selectedElement.Category.Name}\n\n";
 
+         //while (it.MoveNext()) {
+         //   Definition d = it.Key as Definition;
+         //   Binding b = it.Current as Binding;
+         //
+         //   string sbinding = (b is InstanceBinding) ? "instance" : "type";
+         //   data += $"{d.Name} {sbinding}\n\n";
+         //}
+         //TaskDialog.Show("Data", data);
+         //return Result.Succeeded;
+
          try {
             foreach (var p in selectedElemParams) {
                data += $"Parameter name: {(p as Parameter).Definition.Name}\n" +
                    $"Parameter Type: {(p as Parameter).Definition.ParameterType}\n" +
                    $"Parameter Group: {(p as Parameter).Definition.ParameterGroup}\n" +
-                   $"Value: {(p as Parameter).AsDouble()}\n";
+                   $"Value: {(p as Parameter).AsDouble()}\n\n";
 
-               data += ((p as Parameter).Element is FamilyInstance) ? "Parameter ?(instance/type): Instance\n\n" : "Parameter Type: Type\n\n";
-               //data += $"Parameter formula: {(p as GlobalParameter).GetFormula()}\n\n";
+               //data += (typeof(ElementType) != (p as Parameter).Element.GetType()) ? "Parameter ?(instance/type): Instance\n\n" : "Parameter Type: Type\n\n";
+               //data += $"Parameter formula: {(p as FamilyParameter).Formula}\n\n";
+
             }
-
+         
             TaskDialog.Show("Data", data);
             return Result.Succeeded;
+         
          } catch (Exception e) {
             TaskDialog.Show("Exception caught", e.Message);
             return Result.Failed;
