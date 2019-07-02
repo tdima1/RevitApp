d61178197b94c846d4630dc66068c9be935d8151
Dima Tiberiu <dima.tiberiu1@gmail.com>
2019-07-02 13:50:16 +0300

    added formatting settings

--- RevitApp/Main.cs
+++ RevitApp/Main.cs
@@ -6,20 +6,20 @@ using System.Text;
 
 namespace RevitApp
 {
-    class Main : IExternalApplication
-    {
-        public Result OnShutdown(UIControlledApplication application)
-        {
-            return Result.Succeeded;
-        }
+   class Main : IExternalApplication
+   {
+      public Result OnShutdown(UIControlledApplication application)
+      {
+         return Result.Succeeded;
+      }
 
-        public Result OnStartup(UIControlledApplication application)
-        {
-            RibbonPanel panel = application.CreateRibbonPanel("Revit App");
-            PushButton button = panel.AddItem(new PushButtonData($"Lab 1",
-            "Inspect Selected Element", @"RevitApp.dll", $"RevitApp.InspectSelectedElementCommand")) as PushButton;
-            panel.AddSeparator();
-            return Result.Succeeded;
-        }
-    }
+      public Result OnStartup(UIControlledApplication application)
+      {
+         RibbonPanel panel = application.CreateRibbonPanel("Revit App");
+         PushButton button = panel.AddItem(new PushButtonData($"Lab 1",
+         "Inspect Selected Element", @"RevitApp.dll", $"RevitApp.InspectSelectedElementCommand")) as PushButton;
+         panel.AddSeparator();
+         return Result.Succeeded;
+      }
+   }
 }
