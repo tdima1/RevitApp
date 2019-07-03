56c7242d12373561ecdded1cd1ac1407a4108b39
Dima Tiberiu <dima.tiberiu1@gmail.com>
2019-07-03 12:20:48 +0300

    done some formatting

--- RevitApp/InspectSelectedElementCommand.cs
+++ RevitApp/InspectSelectedElementCommand.cs
@@ -27,26 +27,31 @@ namespace RevitApp
 
          ParameterSet selectedElemParams = selectedElement.Parameters;
 
-         data.Append($"Element ID: {selectedElement.Id} \nElement Category Name: {selectedElement.Category.Name}\n\n");
+         data.Append($"Element ID: {selectedElement.Id} </br>\nElement Category Name: {selectedElement.Category.Name}</br></br>\n");
+         data.Append("<style> span { position: absolute; margin-left: 15%; } </style>");
 
          try {
-            foreach (Parameter p in selectedElemParams) {
-               data.Append($"Parameter name: {p.Definition.Name}\n" +
-                   $"Parameter Type: {p.Definition.ParameterType}\n" +
-                   $"Parameter Group: {p.Definition.ParameterGroup}\n" +
-                   $"Value: {p.AsValueString()}\n" +
-                   $"Is Formula: {p.IsReadOnly}\n");
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"\Users\TDima\Desktop\RevitApp\RevitApp\RevitApp\Resources\out.htm", true)) {
+               file.WriteLine($"<h1><center> {data.ToString()} </center></h1></br></br>\n\n");
+               data = new StringBuilder();
 
-               if ((doc.GetElement(p.Element.Id).GetType() != typeof(FamilyInstance))) {
-                  data.Append($"Parameter ?(instance/type): Type\n\n");
-               } else {
-                  if ((doc.GetElement(p.Element.Id).GetType() != typeof(FamilySymbol))) {
-                     data.Append($"Parameter ?(instance/type): Instance\n\n");
+               foreach (Parameter p in selectedElemParams) {
+                  data.Append($"Parameter name: <span style=\"margin-left: 15%\" position=\"absolute\">{p.Definition.Name}</span></br>\n" +
+                      $"Parameter Type: <span style=\"margin-left: 15%\" position=\"absolute\">{p.Definition.ParameterType}</span></br>\n" +
+                      $"Parameter Group: <span style=\"margin-left: 15%\" position=\"absolute\">{p.Definition.ParameterGroup}</span></br>\n" +
+                      $"Value: <span>{p.AsValueString()}</span></br>\n" +
+                      $"Is Formula: <span>{p.IsReadOnly}</span></br>\n");
+
+                  if ((doc.GetElement(p.Element.Id).GetType() != typeof(FamilyInstance))) {
+                     data.Append($"Parameter ?(instance/type): <span style=\"margin-left: 15%\">Type</span></br></br>\n\n");
+                  } else {
+                     if ((doc.GetElement(p.Element.Id).GetType() != typeof(FamilySymbol))) {
+                        data.Append($"Parameter ?(instance/type): <span style=\"margin-left: 15%\">Instance</span></br></br>\n\n");
+                     }
                   }
                }
-            }
-            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"\Users\TDima\Desktop\RevitApp\RevitApp\RevitApp\Resources\out.txt", true)) {
-               file.WriteLine(data.ToString());
+
+               file.WriteLine($"<p> {data.ToString()} </p></br>\n");
                file.Flush();
             }
             return Result.Succeeded;
